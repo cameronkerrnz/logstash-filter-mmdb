7e58824e4425e347015f67c46bd293221fc40662
Cameron Kerr <cameron.kerr@otago.ac.nz>
2020-09-22 12:10:28 +0000

    first working version coded and tested

--- src/main/java/org/logstashplugins/MMDB.java
+++ src/main/java/org/logstashplugins/MMDB.java
@@ -9,8 +9,21 @@ import co.elastic.logstash.api.LogstashPlugin;
 import co.elastic.logstash.api.PluginConfigSpec;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Collection;
 import java.util.Collections;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import com.maxmind.db.Reader;
+import com.maxmind.db.Record;
+import com.maxmind.db.Metadata;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
 
 // class name must match plugin name
 @LogstashPlugin(name = "mmdb")
@@ -18,23 +31,103 @@ public class MMDB implements Filter {
 
     public static final PluginConfigSpec<String> SOURCE_CONFIG =
             PluginConfigSpec.stringSetting("source", "message");
+    public static final PluginConfigSpec<String> TARGET_CONFIG =
+            PluginConfigSpec.stringSetting("target", "mmdb");
+    public static final PluginConfigSpec<String> DATABASE_FILENAME_CONFIG =
+            PluginConfigSpec.stringSetting("database", null);
 
     private String id;
     private String sourceField;
+    private String targetField;
+    private Reader databaseReader;
+    private String databaseFilename;
+    private String failureTag = "_mmdb_lookup_failure";
 
     public MMDB(String id, Configuration config, Context context) {
         // constructors should validate configuration options
         this.id = id;
         this.sourceField = config.get(SOURCE_CONFIG);
+        this.targetField = config.get(TARGET_CONFIG);
+        this.databaseFilename = config.get(DATABASE_FILENAME_CONFIG);
+        
+        if (this.databaseFilename == null) {
+            throw new IllegalStateException("Must specify database filename");
+        }
+
+        File databaseFile = new File(this.databaseFilename);
+        try {
+            this.databaseReader = new Reader(databaseFile); // with no caching (FIXME)
+        } catch (java.io.IOException ex) {
+            throw new IllegalStateException("Database does not appear to be a valid database");
+        }
+
+        context.getLogger(this).info(databaseReader.getMetadata().toString());
+    }
+
+    public Metadata getMetadata() {
+        if (null == this.databaseReader) {
+            return null;
+        }
+        return this.databaseReader.getMetadata();
+    }
+
+    // This assumes that the fields in the MMDB are a flat structure
+    // AND only contain either numbers or strings
+    //
+    private void renderJsonNodeIntoEvent(
+        Iterator<Map.Entry<String,JsonNode>> fields,
+        Event e
+    ) {
+        while (fields.hasNext()) {
+            Map.Entry<String,JsonNode> field = fields.next();
+
+            String key = "[" + this.targetField + "][" + field.getKey() + "]";
+
+            switch (field.getValue().getNodeType()) {
+
+                // https://fasterxml.github.io/jackson-databind/javadoc/2.8/com/fasterxml/jackson/databind/node/JsonNodeType.html
+
+                case STRING:
+                    e.setField(key, field.getValue().textValue());
+                    break;
+
+                case NUMBER:
+                    // This could be a short, int, long, float, double or bignum etc.
+                    // Conversion will depend on what Logstash does
+                    e.setField(key, field.getValue().numberValue());
+                    break;
+
+                default:
+                    e.tag(this.failureTag);
+            }
+        }
     }
 
     @Override
     public Collection<Event> filter(Collection<Event> events, FilterMatchListener matchListener) {
         for (Event e : events) {
-            Object f = e.getField(sourceField);
-            if (f instanceof String) {
-                e.setField(sourceField, StringUtils.reverse((String)f));
+            try {
+                JsonNode recordData = this.databaseReader.get(
+                    InetAddress.getByName(
+                        e.getField(this.sourceField).toString()));
+
+                if (null == recordData) {
+                    e.tag(this.failureTag);
+                    continue;
+                }
+
+                Iterator<Map.Entry<String,JsonNode>> fields = recordData.fields();
+
+                renderJsonNodeIntoEvent(fields, e);
+
                 matchListener.filterMatched(e);
+
+            } catch (java.net.UnknownHostException ex) {
+                e.tag(this.failureTag);
+                continue;
+            } catch (IOException ex) {
+                e.tag(this.failureTag);
+                continue;
             }
         }
         return events;
@@ -43,7 +136,10 @@ public class MMDB implements Filter {
     @Override
     public Collection<PluginConfigSpec<?>> configSchema() {
         // should return a list of all configuration options for this plugin
-        return Collections.singletonList(SOURCE_CONFIG);
+        return Arrays.asList(
+            SOURCE_CONFIG,
+            TARGET_CONFIG,
+            DATABASE_FILENAME_CONFIG);
     }
 
     @Override
--- src/test/java/org/logstashplugins/MMDBTest.java
+++ src/test/java/org/logstashplugins/MMDBTest.java
@@ -4,32 +4,99 @@ import co.elastic.logstash.api.Configuration;
 import co.elastic.logstash.api.Context;
 import co.elastic.logstash.api.Event;
 import co.elastic.logstash.api.FilterMatchListener;
-import org.junit.Assert;
-import org.junit.Test;
 import org.logstash.plugins.ConfigurationImpl;
 import org.logstash.plugins.ContextImpl;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.HashMap;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import com.maxmind.db.Metadata;
+
 public class MMDBTest {
 
     @Test
-    public void testJavaExampleFilter() {
-        String sourceField = "foo";
-        Configuration config = new ConfigurationImpl(Collections.singletonMap("source", sourceField));
+    public void testConfigRequiresDatabase() {
+        
+        HashMap configMap = new HashMap();
+        configMap.put("source", "ip");
+        configMap.put("target", "info");
+        Configuration config = new ConfigurationImpl(configMap);
+        Context context = new ContextImpl(null, null);
+
+        try {
+            MMDB filter = new MMDB("test-id", config, context);
+            fail("Expected an exception to be thrown");
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage(), is("Must specify database filename"));
+        }
+    }
+
+    @Test
+    public void testRequiresValidDatabase() {
+        
+        HashMap configMap = new HashMap();
+        configMap.put("source", "ip");
+        configMap.put("target", "info");
+        configMap.put("database", "samples/demo.mmdb");
+        Configuration config = new ConfigurationImpl(configMap);
+        Context context = new ContextImpl(null, null);
+
+        MMDB filter = new MMDB("test-id", config, context);
+        assertThat(filter.getMetadata().getIpVersion(), is(4));
+        assertThat(filter.getMetadata().getDatabaseType(), is("demo-network"));        
+    }
+
+    @Test
+    public void testDemoBasic() {
+        
+        HashMap configMap = new HashMap();
+        configMap.put("source", "ip");
+        configMap.put("target", "info");
+        configMap.put("database", "samples/demo.mmdb");
+        Configuration config = new ConfigurationImpl(configMap);
         Context context = new ContextImpl(null, null);
         MMDB filter = new MMDB("test-id", config, context);
 
         Event e = new org.logstash.Event();
         TestMatchListener matchListener = new TestMatchListener();
-        e.setField(sourceField, "abcdef");
+        e.setField("ip", "172.16.0.1");
         Collection<Event> results = filter.filter(Collections.singletonList(e), matchListener);
 
-        Assert.assertEquals(1, results.size());
-        Assert.assertEquals("fedcba", e.getField(sourceField));
-        Assert.assertEquals(1, matchListener.getMatchCount());
+        assertNull(e.getField("tags"));
+        assertThat(e.getField("[ip]"), is("172.16.0.1"));
+        assertThat(e.getField("[info][subnet]"), is("172.16.0.0/12"));
+        assertThat(e.getField("[info][name]"), is("DMZ"));
+        assertThat(e.getField("[info][vlan_id]"), is(234L));
+    }
+
+    @Test
+    public void testDemoBasicUnicode() {
+        
+        HashMap configMap = new HashMap();
+        configMap.put("source", "ip");
+        configMap.put("target", "info");
+        configMap.put("database", "samples/demo.mmdb");
+        Configuration config = new ConfigurationImpl(configMap);
+        Context context = new ContextImpl(null, null);
+        MMDB filter = new MMDB("test-id", config, context);
+
+        Event e = new org.logstash.Event();
+        TestMatchListener matchListener = new TestMatchListener();
+        e.setField("ip", "10.64.1.255");
+        Collection<Event> results = filter.filter(Collections.singletonList(e), matchListener);
+
+        assertNull(e.getField("tags"));
+        assertThat(e.getField("[ip]"), is("10.64.1.255"));
+        assertThat(e.getField("[info][subnet]"), is("10.64.0.0/23"));
+        assertThat(e.getField("[info][name]"), is("Unicode NFKC Test (ﬃ)"));
+        assertThat(e.getField("[info][vlan_id]"), is(234L));
     }
 }
 
