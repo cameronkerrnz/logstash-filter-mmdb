a8c51eee208df3f67e0ca37f8391fb92dcaf027c
Cameron Kerr <cameron.kerr@otago.ac.nz>
2020-09-28 03:08:17 +0000

    Adding caching; same as GeoIP

--- src/main/java/org/logstashplugins/MMDB.java
+++ src/main/java/org/logstashplugins/MMDB.java
@@ -7,7 +7,6 @@ import co.elastic.logstash.api.Filter;
 import co.elastic.logstash.api.FilterMatchListener;
 import co.elastic.logstash.api.LogstashPlugin;
 import co.elastic.logstash.api.PluginConfigSpec;
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.Arrays;
 import java.util.Iterator;
@@ -20,6 +19,9 @@ import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.maxmind.db.Reader;
 import com.maxmind.db.Record;
 import com.maxmind.db.Metadata;
+import com.maxmind.db.CHMCache;
+import com.maxmind.db.NoCache;
+import com.maxmind.db.NodeCache;
 
 import java.io.File;
 import java.io.IOException;
@@ -30,18 +32,22 @@ import java.net.InetAddress;
 public class MMDB implements Filter {
 
     public static final PluginConfigSpec<String> SOURCE_CONFIG =
-            PluginConfigSpec.stringSetting("source", "message");
+            PluginConfigSpec.requiredStringSetting("source");
     public static final PluginConfigSpec<String> TARGET_CONFIG =
-            PluginConfigSpec.stringSetting("target", "mmdb");
+            PluginConfigSpec.requiredStringSetting("target");
     public static final PluginConfigSpec<String> DATABASE_FILENAME_CONFIG =
-            PluginConfigSpec.stringSetting("database", null);
-
+            PluginConfigSpec.requiredStringSetting("database");
+    public static final PluginConfigSpec<Long> CACHE_SIZE_CONFIG =
+            PluginConfigSpec.numSetting("cache_size", 4096L);
+    
     private String id;
     private String sourceField;
     private String targetField;
     private Reader databaseReader;
     private String databaseFilename;
     private String failureTag = "_mmdb_lookup_failure";
+
+    private NodeCache cache;
 
     public MMDB(String id, Configuration config, Context context) {
         // constructors should validate configuration options
@@ -53,10 +59,26 @@ public class MMDB implements Filter {
         if (this.databaseFilename == null) {
             throw new IllegalStateException("Must specify database filename");
         }
+        
+        if (this.sourceField == null) {
+            throw new IllegalStateException("Must specify source field");
+        }
+
+        if (this.targetField == null) {
+            throw new IllegalStateException("Must specify target field");
+        }
+
+        if (config.get(CACHE_SIZE_CONFIG) > 0L) {
+            this.cache = new CHMCache(config.get(CACHE_SIZE_CONFIG).intValue());
+        } else if (config.get(CACHE_SIZE_CONFIG) == 0L) {
+            this.cache = NoCache.getInstance();
+        } else {
+            throw new IllegalStateException("Cache size must be either >0 to use a cache, or =0 to use no cache");
+        }
 
         File databaseFile = new File(this.databaseFilename);
         try {
-            this.databaseReader = new Reader(databaseFile); // with no caching (FIXME)
+            this.databaseReader = new Reader(databaseFile, cache);
         } catch (java.io.IOException ex) {
             throw new IllegalStateException("Database does not appear to be a valid database");
         }
@@ -139,7 +161,8 @@ public class MMDB implements Filter {
         return Arrays.asList(
             SOURCE_CONFIG,
             TARGET_CONFIG,
-            DATABASE_FILENAME_CONFIG);
+            DATABASE_FILENAME_CONFIG,
+            CACHE_SIZE_CONFIG);
     }
 
     @Override
--- src/test/java/org/logstashplugins/MMDBTest.java
+++ src/test/java/org/logstashplugins/MMDBTest.java
@@ -12,12 +12,15 @@ import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import com.maxmind.db.Metadata;
+import java.net.InetAddress;
 
 public class MMDBTest {
 
@@ -35,6 +38,59 @@ public class MMDBTest {
             fail("Expected an exception to be thrown");
         } catch (IllegalStateException e) {
             assertThat(e.getMessage(), is("Must specify database filename"));
+        }
+    }
+
+    @Test
+    public void testConfigRequiresSource() {
+        
+        HashMap configMap = new HashMap();
+        configMap.put("target", "info");
+        configMap.put("database", "samples/demo.mmdb");
+        Configuration config = new ConfigurationImpl(configMap);
+        Context context = new ContextImpl(null, null);
+
+        try {
+            MMDB filter = new MMDB("test-id", config, context);
+            fail("Expected an exception to be thrown");
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage(), is("Must specify source field"));
+        }
+    }
+
+    @Test
+    public void testConfigRequiresTarget() {
+        
+        HashMap configMap = new HashMap();
+        configMap.put("source", "message");
+        configMap.put("database", "samples/demo.mmdb");
+        Configuration config = new ConfigurationImpl(configMap);
+        Context context = new ContextImpl(null, null);
+
+        try {
+            MMDB filter = new MMDB("test-id", config, context);
+            fail("Expected an exception to be thrown");
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage(), is("Must specify target field"));
+        }
+    }
+
+    @Test
+    public void testConfigCacheSizeMustNotBeNegative() {
+        
+        HashMap configMap = new HashMap();
+        configMap.put("source", "ip");
+        configMap.put("target", "info");
+        configMap.put("database", "samples/demo.mmdb");
+        configMap.put("cache_size", -1L);
+        Configuration config = new ConfigurationImpl(configMap);
+        Context context = new ContextImpl(null, null);
+
+        try {
+            MMDB filter = new MMDB("test-id", config, context);
+            fail("Expected an exception to be thrown");
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage(), is("Cache size must be either >0 to use a cache, or =0 to use no cache"));
         }
     }
 
@@ -98,6 +154,59 @@ public class MMDBTest {
         assertThat(e.getField("[info][name]"), is("Unicode NFKC Test (ﬃ)"));
         assertThat(e.getField("[info][vlan_id]"), is(234L));
     }
+
+    @Test
+    public void testDemoNoCache() {
+        
+        HashMap configMap = new HashMap();
+        configMap.put("source", "ip");
+        configMap.put("target", "info");
+        configMap.put("database", "samples/demo.mmdb");
+        configMap.put("cache_size", 0L);
+        Configuration config = new ConfigurationImpl(configMap);
+        Context context = new ContextImpl(null, null);
+        MMDB filter = new MMDB("test-id", config, context);
+
+        Event e = new org.logstash.Event();
+        TestMatchListener matchListener = new TestMatchListener();
+        e.setField("ip", "172.16.0.1");
+        Collection<Event> results = filter.filter(Collections.singletonList(e), matchListener);
+
+        assertNull(e.getField("tags"));
+        assertThat(e.getField("[ip]"), is("172.16.0.1"));
+        assertThat(e.getField("[info][subnet]"), is("172.16.0.0/12"));
+        assertThat(e.getField("[info][name]"), is("DMZ"));
+        assertThat(e.getField("[info][vlan_id]"), is(234L));
+    }
+
+    @Test
+    public void testCacheFill() {
+        
+        HashMap configMap = new HashMap();
+        configMap.put("source", "ip");
+        configMap.put("target", "info");
+        configMap.put("database", "samples/demo.mmdb");
+        configMap.put("cache_size", 4L);
+        Configuration config = new ConfigurationImpl(configMap);
+        Context context = new ContextImpl(null, null);
+        MMDB filter = new MMDB("test-id", config, context);
+
+        String ips[] = new String[] {
+            "10.10.1.1", "10.10.1.2", "10.10.1.3", "10.10.1.4", "10.10.1.5"
+        };
+
+        TestMatchListener matchListener = new TestMatchListener();
+
+        for (String ip : ips) {
+            Event e = new org.logstash.Event();
+            e.setField("ip", ip);
+            Collection<Event> results = filter.filter(Collections.singletonList(e), matchListener);
+
+            assertNull(e.getField("tags"));
+            assertThat(e.getField("ip"), is(ip));
+        }
+    }
+
 }
 
 class TestMatchListener implements FilterMatchListener {
