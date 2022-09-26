b77cb3982c42390ff90e21405a66e473d5af5b33
Cameron Kerr <cameron.kerr.nz@gmail.com>
2022-09-26 09:15:40 +0000

    fixup 7.9

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo LOGSTASH_CORE_PATH=/src/logstash/logstash-core > ./gradlew.properties
-docker run --rm -it -v $PWD:/work cameronkerrnz/logstash-plugin-dev:7.9 \
+echo LOGSTASH_CORE_PATH=/src/logstash/logstash-core > ./gradle.properties
+#docker run --rm -it -v $PWD:/work cameronkerrnz/logstash-plugin-dev:7.9 \
+docker run --rm -it -v $PWD:/work logstash-plugin-dev:7.9 \
   -c "
     chmod +x ./gradlew \
     && ./gradlew assemble \
